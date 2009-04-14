d75fc029dbf9e0dc82965d1a9741deb6c121be06
Sergej Braznikov <sb@ryotic.de>
2009-04-14 10:41:23 +0200

    info.py nach neue format gestaltet

--- info.py
+++ info.py
@@ -1,46 +1,64 @@
 # encoding: utf-8
 
+# =============================================================================
 # package info
-name = 'symmetrics_module_delivery_time'
-tags = ('magento', 'module')
+# =============================================================================
+NAME = 'symmetrics_module_delivery_time'
 
-# relations
-requires = {
-}
-excludes = {
-}
+TAGS = ('magento', 'module', 'delivery time', 'symmetrics')
 
-# who is responsible for this package?
-team_leader = {
+LICENSE = 'AFL 3.0'
+
+HOMEPAGE = 'http://www.symmetrics.de'
+
+INSTALL_PATH = ''
+
+# =============================================================================
+# responsibilities
+# =============================================================================
+TEAM_LEADER = {
     'Sergej Braznikov': 'sb@symmetrics.de'
 }
 
-# who should check this package in the first place?
-maintainer = {
+MAINTAINER = {
     'Sergej Braznikov': 'sb@symmetrics.de'
 }
 
-# relative installation path (e.g. app/code/local)
-install_path = ''
+AUTHORS = {
+    'Sergej Braznikov': 'sb@symmetrics.de'
+}
 
+# =============================================================================
 # additional infos
-info = 'Lieferzeit für Produkte'
-summary = '''
-    Jedes Produkt im Adminbereich (Backend) verfügt über ein neues Attribut "Lieferzeit".
-    Zusätzlich wird das Attribut in Produktübersicht als neue Spalte dargestellt.
-'''
-notes = '''
-    [special notes, restrictions, bugs etc.]
-'''
-license = 'AFL 3.0'
-authors = {
-    'Sergej Braznikov': 'sb@symmetrics.de'
-}
-homepage = 'http://www.symmetrics.de'
+# =============================================================================
+INFO = 'Lieferzeit für Produkte'
 
-# files this package depends on
-depends_on_files = (
+SUMMARY = '''
+    Jedes Produkt im Adminbereich (Backend) verfügt über ein neues Attribut
+    "Lieferzeit". Zusätzlich wird das Attribut in Produktübersicht als neue
+    Spalte dargestellt.
+'''
+
+NOTES = '''
+'''
+
+# =============================================================================
+# relations
+# =============================================================================
+REQUIRES = {
+}
+
+EXCLUDES = {
+}
+
+DEPENDS_ON_FILES = (
     'app/core/code/Mage/Adminhtml/Block/Catalog/Product/Grid.php',
     'app/core/code/Mage/Sales/Block/Order/Item/Renderer/Default.php',
     'app/core/code/Mage/Eav/Model/Entity/Setup.php',
 )
+
+PEAR_KEY = ''
+
+COMPATIBLE_WITH = {
+    'magento': '1.3.0'
+}
