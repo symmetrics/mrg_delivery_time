ca5a931be93570cbedd0b306e5bc93fe683e4148
Sergej Braznikov <sb@ryotic.de>
2009-06-18 15:02:09 +0200

    Problem mit Übersetzung behoben.

--- build/app/code/local/Symmetrics/DeliveryTime/sql/deliverytime_setup/mysql4-install-0.1.0.php
+++ build/app/code/local/Symmetrics/DeliveryTime/sql/deliverytime_setup/mysql4-install-0.1.0.php
@@ -6,7 +6,7 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->addAttribute('catalog_product', 'delivery_time', array(
-    'label' => 'Delivery Time',
+    'label' => 'Lieferzeit',
     'input' => 'text',
     'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
     'visible' => true,
@@ -19,4 +19,12 @@ $installer->addAttribute('catalog_product', 'delivery_time', array(
     'default' => '2-3 Tage',
 ));
 
+// NOTE: temporary fix for translate problem
+// TODO: fix this!
+$query = <<< EOF
+    INSERT INTO `core_translate` (`key_id`, `string`, `store_id`, `translate`, `locale`)
+    VALUES (NULL , '', '0', '', 'en_US'), (NULL , 'Symmetrics_DeliveryTime::Delivery time', '0', 'Lieferzeit', 'de_DE');
+EOF;
+
+$this->run($query);
 $installer->endSetup();
