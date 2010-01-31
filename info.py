c8ef7950702f03591b7f55df69b8027d58681b3e
Eugen Gitin <eg@symmetrics.de>
2010-01-31 19:33:33 +0100

    Kompatibilität zu CE 1.4 und EE 1.7 hinzugefügt

--- info.py
+++ info.py
@@ -60,6 +60,6 @@ DEPENDS_ON_FILES = (
 PEAR_KEY = ''
 
 COMPATIBLE_WITH = {
-    'magento': ['1.3.2.1', '1.3.2.3', '1.3.2.4'],
-    'magento_enterprise': ['1.3.2.1', '1.3.2.3', '1.3.2.4'],
+    'magento': ['1.3.2.1', '1.3.2.3', '1.3.2.4', '1.4.0.0'],
+    'magento_enterprise': ['1.3.2.1', '1.3.2.3', '1.3.2.4', '1.7.0.0'],
 }
